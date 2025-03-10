d9279350e0596af7d6b46f0888556e2eb4393929
Zuki1273 <barito279@gmail.com>
2025-03-10 13:06:49 +0800

    membuat file prodi.php

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
     <br>
     <table border="1" cellspacing="0" cellpadding="5">
         <thead>
-            <td>No</td>
-            <td>NIM</td>
-            <td>Nama</td>
+            <th>No</th>
+            <th>NIM</th>
+            <th>Nama</th>
         </thead>
         <tbody>
             <tr>
-                <th>7</th>
+                <td>7</td>
                 <td>E020323056</td>
                 <td>Zukimi</td>
             </tr>
