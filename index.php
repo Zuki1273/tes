b34a809d91be20560359e888764e47a68d64e2cd
Zuki1273 <barito279@gmail.com>
2025-03-10 13:05:32 +0800

    membuat file prodi.php

--- index.php
+++ index.php
@@ -7,5 +7,20 @@
 </head>
 <body>
     <h1>Data Mahasiswa</h1>
+    <br>
+    <table border="1" cellspacing="0" cellpadding="5">
+        <thead>
+            <td>No</td>
+            <td>NIM</td>
+            <td>Nama</td>
+        </thead>
+        <tbody>
+            <tr>
+                <th>7</th>
+                <td>E020323056</td>
+                <td>Zukimi</td>
+            </tr>
+        </tbody>
+    </table>
 </body>
 </html>
