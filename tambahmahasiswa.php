c17789924c00a3dda0ff1ae1c584cf3a8a9a58d1
Zuki1273 <barito279@gmail.com>
2025-04-21 21:38:57 +0800

    Create Mahasiswa

--- tambahmahasiswa.php
+++ tambahmahasiswa.php
@@ -10,7 +10,7 @@ $data = ambildata($query);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah Mahasiswa</title>
+    <title>Tambah Data Mahasiswa</title>
 </head>
 
 <body>
